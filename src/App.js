0d6533802242b005b25fbbac0b3ff567eeae4f42
virghiu <virghiu@gmail.com>
2021-07-29 18:26:35 -0400

    header design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Lets build gmail</h1>
+      <Header />
+      <h1>Lets build gmail.</h1>
     </div>
   );
 }
