0f9ba55f3947b9e6f53177b07f9ddd5b3bc00aef
virghiu <virghiu@gmail.com>
2021-08-02 10:43:19 -0400

    sidebar complete

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './SidebarOption.css';
 
-export default function SidebarOption({ Icon, title, number }) {
+export default function SidebarOption({ Icon, title, number, selected }) {
   return (
-    <div className='sidebarOption'>
+    <div className={`sidebarOption ${selected && 'sidebarOption--active'}`}>
       <Icon />
       <h3>{title}</h3>
       <p>{number}</p>
